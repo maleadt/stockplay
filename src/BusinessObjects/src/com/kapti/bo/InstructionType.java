42453622ca750155d645b7d24403e90f05cefdd7
Thijs Walcarius <thijs.walcarius@gmail.com>
2010-02-23 15:26:43 +0000

    Basis van businessobjects, zonder logica

---
+++ src/BusinessObjects/src/com/kapti/bo/InstructionType.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.kapti.bo;
+
+/**
+ *
+ * @author Thijs
+ */
+public enum InstructionType {
+    BUY,
+    SELL,
+    CANCEL,
+    BUY_IMMIDIATE,
+    SELL_IMMIDIATE
+
+}
---
+++ src/BusinessObjects/src/com/kapti/bo/interfaces/IExchange.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.kapti.bo.interfaces;
+
+/**
+ *
+ * @author Thijs
+ */
+public interface IExchange {
+
+    String getLocation();
+
+    String getName();
+
+    String getSymbol();
+
+}
